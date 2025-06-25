76cc97bebb77b20af4d6b165417b63e642aebf0f
Akshayjg2204 <Akshayjg2204@gmail.com>
2025-06-25 13:45:55 +0530

    Add protected routes and authentication guards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import ProfilePage from './pages/Profile/profile'
 import { CommunityDashboard } from './components/community/community-dashboard'
 import { CommunityDetail } from './components/community/community-detail'
 import StudyPlan from './pages/StudyPlan/StudyPlan'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
@@ -21,16 +22,16 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/callback" element={<div>Loading...</div>} />
-      <Route path="/aiassistant" element={<ChatDashboard/>}/>
+      <Route path="/aiassistant" element={<ProtectedRoute><ChatDashboard/></ProtectedRoute>}/>
       <Route path="/courses" element={<CourseList courses={coursesData} />} />
       <Route path="/courses/:id" element={<CourseDetail courses={coursesData} />} />
       <Route path="/courses/:id/learn" element={<CourseContent courses={coursesData} />} />
       <Route path="/courses/:id/exam" element={<ExamPage courses={coursesData} />} />
       <Route path="/courseQuiz" element={<InterestQuiz courses={coursesData} />} />
-      <Route path="/profile" element={<ProfilePage/>} />
-      <Route path="/communities" element={<CommunityDashboard/>} />
-      <Route path="/communities/:id" element={<CommunityDetail />} />
-      <Route path="/studyplan" element={<StudyPlan />} />
+      <Route path="/profile" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>} />
+      <Route path="/communities" element={<ProtectedRoute><CommunityDashboard/></ProtectedRoute>} />
+      <Route path="/communities/:id" element={<ProtectedRoute><CommunityDetail /></ProtectedRoute>} />
+      <Route path="/studyplan" element={<ProtectedRoute><StudyPlan /></ProtectedRoute>} />
     </Routes>
     </>
   )
