338cc6862bfdcf648e2cfc9a9988546c95e656ce
Akshayjg2204 <Akshayjg2204@gmail.com>
2025-06-25 13:22:36 +0530

    Update Auth0 configuration with correct Netlify URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,27 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from "react-router-dom"
+import { Auth0Provider } from '@auth0/auth0-react'
 import { getConfig } from './utils/auth-config'
 
 const config = getConfig();
 
-createRoot(document.getElementById('root')).render(
-  <Auth0Provider
-    domain={config.domain}
-    clientId={config.clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-      ...(config.audience ? { audience: config.audience } : {})
-    }}
-    cacheLocation="localstorage"
-    useRefreshTokens={true}
-  >
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Auth0Provider>
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  authorizationParams: {
+    redirect_uri: "https://ailms1.netlify.app",
+  },
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Auth0Provider {...providerConfig}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
+  </React.StrictMode>,
 )
